1f6b2dfffc5a73a6ef58c2dae97e3d87e13ec455
Fredrik Eilertsen <eilertsenfredrik@gmail.com>
2019-09-28 22:27:59 +0200

    Fix for memory leak on Android 10 when backing out of the app

--- TodoList/app/src/main/java/com/github/fredrik9000/todolist/MainActivity.java
+++ TodoList/app/src/main/java/com/github/fredrik9000/todolist/MainActivity.java
@@ -1,5 +1,6 @@
 package com.github.fredrik9000.todolist;
 
+import android.os.Build;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -22,5 +23,20 @@ public class MainActivity extends AppCompatActivity {
     public boolean onSupportNavigateUp() {
         NavController controller = Navigation.findNavController(this, R.id.nav_host_fragment);
         return controller.navigateUp();
+    }
+
+    @Override
+    public void onBackPressed() {
+        // Fix for memory leak in the Android framework that happens on Android 10:
+        // https://issuetracker.google.com/issues/139738913
+        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q
+                && isTaskRoot()
+                && getSupportFragmentManager().getPrimaryNavigationFragment() != null
+                && getSupportFragmentManager().getPrimaryNavigationFragment().getChildFragmentManager().getBackStackEntryCount() == 0
+                && getSupportFragmentManager().getBackStackEntryCount() == 0) {
+            finishAfterTransition();
+        } else {
+            super.onBackPressed();
+        }
     }
 }
